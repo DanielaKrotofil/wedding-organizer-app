53f11b49c3010bb0683ebcd0865f5bc68e142dd2
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-03-24 18:54:30 +0100

    about app and footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { HashRouter, Route, Routes, Link, NavLink, Outlet} from 'react-router-do
 import Home from './Home';
 import Vision from './Vision';
 import Guests from './Guests';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faList, faWifi, faPiggyBank, faFaceGrinHearts } from '@fortawesome/free-solid-svg-icons';
 
 const Layout = () => {
 
@@ -40,20 +42,49 @@ function App() {
     <section className='banner'>
       <h1>Wedding <span>Planner</span> </h1>
     </section>
+
+    <section className='container about'>
+      <h2>About App</h2>
+      <div className="about__app">
+        <div className="about__app__item">
+          <span className="about__app__item_img">
+            <FontAwesomeIcon icon={faList} />
+          </span>
+          <h3>Lists</h3>
+          <p>You can find here to-do list, list of guests and more!</p>
+        </div>
+
+        <div className="about__app__item">
+          <span className="about__app__item_img">
+            <FontAwesomeIcon icon={faFaceGrinHearts} />
+          </span>
+          <h3>Help</h3>
+          <p>Help with rganiztion of wedding with pleasure: formal tips, styling tips, decorating tips and more</p>
+        </div>
+
+        <div className="about__app__item">
+          <span className="about__app__item_img">
+            <FontAwesomeIcon icon={faWifi} />
+          </span>
+          <h3>Internet</h3>
+          <p>To use this app you need to be connected with the Internet</p>
+        </div>
+
+        <div className="about__app__item">
+          <span className="about__app__item_img">
+            <FontAwesomeIcon icon={faPiggyBank} />
+          </span>
+          <h3>Free</h3>
+          <p>No need money to use this app</p>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <span>© 2023 Wedding Organizer</span>
+    </footer>
     </>
   )
 }
 
 export default App
-
-/*
-    <HashRouter>
-            <Routes>
-                <Route element={<Layout/>}>
-                    <Route path='/' element={<Home/>}/>
-                    <Route path='blog' element={<Blog/>}/>
-                    <Route path="pricing" element={<Pricing/>}/>
-                </Route>
-            </Routes>
-      </HashRouter>
-*/
