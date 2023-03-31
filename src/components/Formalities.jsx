5bf3f48a0a52390f6a46e3ff0cdb05e0db43366b
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-03-31 19:50:16 +0200

    formalities

--- src/components/Formalities.jsx
+++ src/components/Formalities.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import {useState} from "react";
 
 const Formalities = () => {
-    return <h1>Formalities</h1>
+    const [todoList, setTodoList] = useState([
+        {
+            id: 1,
+            name: "Booking the wedding date and time at the registry office",
+            done: false
+        },
+        {
+            id: 2,
+            name: "Stamp duty",
+            done: true
+        },
+        {
+            id: 3,
+            name: "Outdoor wedding fees",
+            done: false
+        },
+        {
+            id: 4,
+            name: "Booking a date in the church",
+            done: false
+        }
+    ]);
+
+    const [inputValue, setInputValue] = useState('');
+
+    const updateInputValue = ({target}) => setInputValue(target.value)
+
+    const onAddNewTodo = (e) => {
+        e.preventDefault()
+        setTodoList(prevState => {
+            const newEl = {
+                id: prevState.length + 1,
+                name: inputValue,
+                done: false,
+            }
+            return [...prevState, newEl]
+        })
+    }
+
+    const onTodoClick = (idToChange) => {
+        setTodoList(prevState => {
+            const prevStateCP = [...prevState]
+
+            const indexToChange = prevStateCP.findIndex(({ id }) => id === idToChange);
+            prevStateCP[indexToChange] = { ...prevStateCP[indexToChange], done: !prevStateCP[indexToChange].done }
+
+            return prevStateCP
+        })
+    }
+
+    return (
+        <div className="toDoList">
+            <form className="header" onSubmit={onAddNewTodo}>
+                <h2>Formalities List</h2>
+                <input value={inputValue} onChange={updateInputValue} type="text" placeholder="example: Order Of Wedding Ceremony"/>
+                <button className="btn-add" type='submit'>Dodaj</button>
+            </form>
+
+            <ul>
+                {
+                    todoList.map(todo => <li key={todo.id} onClick={() => onTodoClick(todo.id)} className={todo.done ? 'done' : ''}>{todo.name}</li>)
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default Formalities
