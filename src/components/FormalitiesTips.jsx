c81df19765d2e2e71bd8846a7547135090945abd
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-04-01 12:52:56 +0200

    formlities components

---
+++ src/components/FormalitiesTips.jsx
@@ -0,0 +1,9 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../firebase';
+
+const FormalitiesTips = () => {
+    return (<>
+    <h2>Tips</h2>
+    </>)
+}
+export default FormalitiesTips
