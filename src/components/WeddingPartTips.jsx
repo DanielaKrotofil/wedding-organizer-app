f5e2a9ebe07ef785bf2ce79c492e18c05028a3ec
DanielaKrotofil <112739341+DanielaKrotofil@users.noreply.github.com>
2023-04-08 16:56:48 +0200

    wedding party update

--- src/components/WeddingPartTips.jsx
+++ src/components/WeddingPartTips.jsx
@@ -4,7 +4,10 @@ import tableinformal from '../assets/table-setting-informal.jpg';
 const Plates = () => {
     const simplestyle = ['boho', 'greenry', 'rustic'];
     const restlestyle = ['glamour', 'shabby chic', 'classic/traditional']
+
     return (<>
+    <h2>Plates</h2>
+
     <div className="weddingparty__tips__tablesetting">
         <div className="weddingparty__tips__tablesetting__box">
             <span className='weddingparty__tips__tablesetting__box-text'>
@@ -22,20 +25,23 @@ const Plates = () => {
             <img src={tableinformal} alt="Informal" />
         </div>
     </div>
+
     </>)
 }
 
 const PartyTipsTable = () => {
     return (<>
         <div className='weddingparty__tips__arrangement'>
-            <h3>Table layout is important</h3>
-            <span>
+            <h2>Table layout</h2>
+
+            <span className="text">
                 <p>You have many options for the arrangement of tables, but you have to adapt the arrangement to the number of guests and space in the room</p>
                 <p>See below for some table layout suggestions</p>
             </span>
             <img src="https://i.pinimg.com/564x/7a/8e/d2/7a8ed2eec63bd42058d16df07b070776.jpg" alt="table-layouts" />
             <a href="https://pl.pinterest.com/pin/68744883649/" className="btn">click here for more inspirations</a>
         </div>
+
         <Plates/>
     </>)
 }
--- src/components/WeddingPartyHalls.jsx
+++ src/components/WeddingPartyHalls.jsx
@@ -1,26 +1,34 @@
 import { halls} from "../data/halls"
-import {useEffect, useState} from 'react';
 import HallsGetInfo from "./WeddingHalsGetInfo";
 
 const WeddingHals = () => {
     console.log(halls)
 
     return (<div className="weddingparty__halls">
-    <h2>Wedding Halls</h2>
-    <div>
+        <h2>Wedding Halls</h2>
 
-        <div className="text">
-            <p>What is important to find the best hall?</p>
-            <p>Visit several halls and take notes from up to 4 rooms that impressed you the most.</p>
+        <div>
+            <div className="text">
+                <p>What is important to find the best hall?</p>
+                <p>Visit several halls and take notes from up to 4 rooms that impressed you the most.</p>
+            </div>
+
+            <HallsGetInfo/>
+
+            <div className="text">
+                <p className="text-paragraph">Remember that the wedding hall is supposed to please you! Below are examples of wedding halls in Poland - take a look, maybe one of them will be the perfect choice?</p>
+            </div>
+
+            <div className="weddingparty__halls-examples">
+            {halls.map(halls => <div key={halls.id} onClick={() => addHall(halls.id)}>
+                    <p>{halls.name}</p>
+                    <p>{halls.address}</p>
+                    <p>{halls.phone}</p>
+                    <p>{halls.web}</p>
+                </div>)
+            }
         </div>
-
-        <HallsGetInfo/>
-
-        <div className="text">
-            <p className="text-paragraph">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia inventore sunt a ipsam commodi dolor alias nihil qui, delectus voluptatibus ab nesciunt praesentium dolorum non soluta repellendus? Quae, quis veniam.</p>
         </div>
-
-    </div>
     </div>)
 }
 export default WeddingHals
